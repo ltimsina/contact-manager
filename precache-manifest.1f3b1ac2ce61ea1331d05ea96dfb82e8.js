eb3561e5cfdeac0fcfaf3abef02aff9b7cea1c86
Lek timsina <lt098565@vodafone.com.au>
2019-12-16 12:29:34 +1100

    Updates

---
+++ precache-manifest.1f3b1ac2ce61ea1331d05ea96dfb82e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f86304e13484ffd32964e15af20e594",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "681ecf4768ba6276dd61",
+    "url": "/contact-manager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "bfd47baac882009afb3f",
+    "url": "/contact-manager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "681ecf4768ba6276dd61",
+    "url": "/contact-manager/static/js/2.a4a18f4e.chunk.js"
+  },
+  {
+    "revision": "bfd47baac882009afb3f",
+    "url": "/contact-manager/static/js/main.c6c7a9d8.chunk.js"
+  },
+  {
+    "revision": "60947100546d1353dea8",
+    "url": "/contact-manager/static/js/runtime-main.b290a8ae.js"
+  }
+]);
